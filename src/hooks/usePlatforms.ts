a1123623835d72561efe1a7688f1599476e60bdf
enthuSoftCoder <softenthubiplob@gmail.com>
2023-10-25 21:23:57 +0600

    Create a reusable api client

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,20 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Platform>>("/platform/lists/parents")
-        .then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
     initialData: { count: platforms.length, results: platforms },
   });
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,22 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "e753593b00ba43d28ede2be1d13f317e",
   },
 });
+
+export default class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) =>
+    axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+}

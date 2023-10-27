70ba2af291ccd7149da8fa1664c82d01651b275e
enthuSoftCoder <softenthubiplob@gmail.com>
2023-10-28 04:39:00 +0600

    remove unused file

--- src/hooks/useTrailer.1.ts
+++
@@ -1,9 +0,0 @@
-import { Trailer } from "../entities/Trailer";
-import APIClient from "../services/api-client";
-
-const useTrailer = (id: number) => {
-  const apiClient = new APIClient<Trailer>(`/games/${id}/movies`);
-  useQuery({
-    qF,
-  });
-};
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
-import "./index.css";
-import theme from "./theme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
+import "./index.css";
 import routes from "./routes.tsx";
+import theme from "./theme.ts";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={routes} />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>

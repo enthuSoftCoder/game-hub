b55fbcfc7c5e9bf6d3b320a522c5e0cc99ac19d3
enthuSoftCoder <softenthubiplob@gmail.com>
2023-10-28 03:01:44 +0600

    Refactor: move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,12 +3,8 @@ import genres from "../data/genres";
 
 import APIClient from "../services/api-client";
 import ms from "ms";
+import { Genre } from "../entities/Genre";
 
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 const apiClient = new APIClient<Genre>("/genres");
 const useGenres = () =>
   useQuery({
